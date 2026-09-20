7dec0be5f02c7b7581d9730b368e835de6cc68c3
josephsanthoshk <josephsanthosh192@gmail.com>
2026-09-20 21:41:23 +0530

    Update tt_um_regex_matcher.v

--- src/tt_um_regex_matcher.v
+++ src/tt_um_regex_matcher.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_noel_regex_matcher (
+module tt_um_regex_matcher (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
